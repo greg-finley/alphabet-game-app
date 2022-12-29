5e7137b897265466c6b60955662a646978d4f009
Greg Finley <gregory.finley@gmail.com>
2022-12-29 10:23:49 -0800

    Start fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    fetch(`https://us-central1-greg-finley.cloudfunctions.net/alphabet-game-plays-api?matches_only=true&limit=2&sport=NFL&before_ts=1667525177`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        } 
+        return response.json();
+      })
+      .then((actualData) => {
+        console.log(actualData);
+        setData(actualData);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {loading && <div>A moment please...</div>}
+        {data && <div>{JSON.stringify(data)}</div>}
+        {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
         <p>
           TypeScript! Edit <code>src/App.tsx</code> and save to reload.
         </p>

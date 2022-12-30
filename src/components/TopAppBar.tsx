2d0cfec948aeb23d79c2be8bb5b577ed8275b54d
Greg Finley <gregory.finley@gmail.com>
2022-12-29 23:27:52 -0800

    Rework top bar

--- src/components/TopAppBar.tsx
+++ src/components/TopAppBar.tsx
@@ -2,33 +2,28 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 
 function TopAppBar() {
-
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <div><img src="logo192.png" alt="Icon" height={'35vh'} /></div>
-          <Typography
-            variant="h6"
-            noWrap
-            sx={{
-              mr: 2,
-              ml: 2,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              color: 'inherit',
-              textDecoration: 'none',
-              fontSize: '5vw',
-            }}
-          >
-            Sports Alphabet Game
-          </Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <AppBar component="nav" position="sticky">
+    <Toolbar>
+    <div><a href='/'><img src="logo192.png" alt="Icon" height={'35vh'} /></a></div>
+      <Typography
+        variant="h6"
+        component="div"
+        sx={{
+          ml: 2,
+          fontFamily: 'monospace',
+          fontWeight: 700,
+          color: 'inherit',
+          textDecoration: 'none',
+          fontSize: '1.2rem',
+        }}
+      >
+        Sports Alphabet Game
+      </Typography>
+    </Toolbar>
+  </AppBar>
   );
 }
 export default TopAppBar;

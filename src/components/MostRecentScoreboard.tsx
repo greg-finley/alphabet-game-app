5215ee32ed15299d1000d332b0eb54d4100f9988
Greg Finley <gregory.finley@gmail.com>
2022-12-31 16:10:48 -0800

    Progress

--- src/components/MostRecentScoreboard.tsx
+++ src/components/MostRecentScoreboard.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
-import { MostRecentScores, Play } from "../types";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-
-dayjs.extend(relativeTime);
+import { MostRecentScores } from "../types";
+import ScoreboardCard from "./ScoreboardCard";
 
 interface MostRecentScoresProps {
   mostRecentScores: MostRecentScores;
@@ -13,46 +10,13 @@ function MostRecentScoreboard(props: MostRecentScoresProps) {
   const { mostRecentScores } = props;
   return (
     <>
-      <div>{Object.values(mostRecentScores).map((x) => playerImage(x))}</div>
+      <div>
+        {Object.values(mostRecentScores).map((play) => (
+          <ScoreboardCard play={play} key={play.sport + play.player_id} />
+        ))}
+      </div>
     </>
   );
 }
-
-const playerImage = (play: Play) => {
-  let src = "";
-  switch (play.sport) {
-    case "NHL":
-      src = `https://cms.nhl.bamgrid.com/images/headshots/current/168x168/${play.player_id}.jpg`;
-      break;
-    case "NFL":
-      src = `https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/${play.player_id}.png&h=110&w=110&scale=crop`;
-      break;
-    case "NBA":
-      src = `https://cdn.nba.com/headshots/nba/latest/1040x760/${play.player_id}.png?imwidth=104&imheight=76`;
-      break;
-    case "MLB":
-      src = `https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/h_1000,q_auto:best/v1/people/${play.player_id}/headshot/67/current`;
-      break;
-  }
-  return (
-    <div className="Player-image-container" key={play.sport + play.player_id}>
-      <img
-        className="Player-image"
-        src={src}
-        alt={play.player_name}
-        onError={({ currentTarget }) => {
-          currentTarget.onerror = null;
-          currentTarget.src =
-            "https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&h=110&w=110&scale=crop";
-        }}
-      />
-      <div>{play.next_letter}</div>
-      <div>{play.matching_letters.join(", ")}</div>
-      <div>{play.times_cycled}</div>
-      <div>{play.completed_at}</div>
-      <div>{dayjs().to(dayjs.unix(play.completed_at))}</div>
-    </div>
-  );
-};
 
 export default MostRecentScoreboard;
---
+++ src/components/ScoreboardCard.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { Play, Sport } from "../types";
+import { TwitterFollowButton } from "react-twitter-embed";
+
+dayjs.extend(relativeTime);
+
+interface ScoreboardCardProps {
+  play: Play;
+}
+
+export default function ScoreboardCard(props: ScoreboardCardProps) {
+  const { play } = props;
+  return (
+    <Card sx={{ display: "flex" }}>
+      <div className="Player-image-container">
+        <img
+          className="Player-image"
+          src={playerImageSrc(play)}
+          alt={play.player_name}
+          onError={({ currentTarget }) => {
+            currentTarget.onerror = null;
+            currentTarget.src =
+              "https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&h=110&w=110&scale=crop";
+          }}
+        />
+      </div>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "left",
+        }}
+      >
+        <CardContent sx={{ flex: "1 0 auto", textAlign: "left" }}>
+          <Typography variant="h6">{play.player_name}</Typography>
+          <Typography>{sportScore(play.sport) + "!"}</Typography>
+          <Typography>
+            His name has the letter
+            {play.matching_letters.length === 1 ? "" : "s"}{" "}
+            {play.matching_letters.join(", ")}.
+          </Typography>
+          <Typography>Next letter: {play.next_letter}.</Typography>
+          <Typography>
+            Cycled {play.times_cycled} {play.season_phrase}.
+          </Typography>
+          <Typography>{dayjs().to(dayjs.unix(play.completed_at))}</Typography>
+          <TwitterFollowButton
+            screenName={`${play.sport}AlphabetGame`}
+            options={{ showCount: false }}
+          />
+        </CardContent>
+      </Box>
+    </Card>
+  );
+}
+
+const playerImageSrc = (play: Play) => {
+  switch (play.sport) {
+    case "NHL":
+      return `https://cms.nhl.bamgrid.com/images/headshots/current/168x168/${play.player_id}.jpg`;
+    case "NFL":
+      return `https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/${play.player_id}.png&h=110&w=110&scale=crop`;
+    case "NBA":
+      return `https://cdn.nba.com/headshots/nba/latest/1040x760/${play.player_id}.png?imwidth=104&imheight=76`;
+    case "MLB":
+      return `https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/h_1000,q_auto:best/v1/people/${play.player_id}/headshot/67/current`;
+  }
+};
+
+const sportScore = (sport: Sport) => {
+  switch (sport) {
+    case "NHL":
+      return "Goal";
+    case "NFL":
+      return "Touchdown";
+    case "NBA":
+      return "Dunk";
+    case "MLB":
+      return "Home run";
+  }
+};

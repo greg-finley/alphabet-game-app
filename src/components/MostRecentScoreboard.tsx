0d5288fe0d5a96b8905d913ad6ae64cc87cc45b8
Greg Finley <gregory.finley@gmail.com>
2023-01-06 22:00:00 -0800

    Temporarily nix styles so that the filters sit atop the buttons

--- src/components/MostRecentScoreboard.tsx
+++ src/components/MostRecentScoreboard.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Play, sports } from "../types";
 import ScoreboardCard from "./ScoreboardCard";
-import styles from "./MostRecentScoreboard.module.css";
 import InfiniteScroll from "react-infinite-scroll-component";
 import LoadingCircle from "./LoadingCircle";
 import { Avatar } from "@mui/material";
@@ -53,45 +52,46 @@ function MostRecentScoreboard(props: MostRecentScoresProps) {
   };
 
   return (
-    <>
-      <div className={styles.grid}>
-        <Tabs
-          value={tabIndex}
-          onChange={handleTabClick}
-          centered
-          sx={{
-            maxWidth: "500px",
-          }}
+    <div>
+      <Tabs
+        value={tabIndex}
+        onChange={handleTabClick}
+        centered
+        sx={{
+          maxWidth: "500px",
+        }}
+      >
+        {sports.map((sport, index) => (
+          <Tab
+            icon={<Avatar src={`${sport}.jpeg`} variant="square" />}
+            key={sport}
+            sx={{
+              backgroundColor: tabIndex === index ? "currentColor" : "#B7CEF3",
+              border: "1px solid black",
+            }}
+          />
+        ))}
+      </Tabs>
+      {items.length ? (
+        <InfiniteScroll
+          dataLength={items.length}
+          next={fetchData}
+          hasMore={items.length < plays.length}
+          loader={<LoadingCircle />}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
         >
-          {sports.map((sport) => (
-            <Tab
-              icon={<Avatar src={`${sport}.jpeg`} variant="square" />}
-              key={sport}
-              sx={{ color: "white", border: "1px solid black" }}
-            />
-          ))}
-        </Tabs>
-        {items.length ? (
-          <InfiniteScroll
-            dataLength={items.length}
-            next={fetchData}
-            hasMore={items.length < plays.length}
-            loader={<LoadingCircle />}
-            endMessage={
-              <p style={{ textAlign: "center" }}>
-                <b>Yay! You have seen it all</b>
-              </p>
-            }
-          >
-            {items}
-          </InfiniteScroll>
-        ) : (
-          <p style={{ textAlign: "center" }}>
-            <b>See you when the season starts!</b>
-          </p>
-        )}
-      </div>
-    </>
+          {items}
+        </InfiniteScroll>
+      ) : (
+        <p style={{ textAlign: "center" }}>
+          <b>See you when the season starts!</b>
+        </p>
+      )}
+    </div>
   );
 }
 

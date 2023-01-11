1fc466384568592396cc683c51c46ed2cfdb38ec
Greg Finley <gregory.finley@gmail.com>
2023-01-10 23:16:11 -0800

    Improve

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import TopAppBar from "../components/TopAppBar";
 import { State } from "../types";
 import styles from "./Home.module.css";
 import { ScrollRestoration } from "react-router-dom";
-import ScoreBox from "../components/ScoreBox";
+import "../fonts/ds-digital.css";
 
 interface HomeProps {
   state: State;
@@ -18,6 +18,20 @@ export default function Home(props: HomeProps) {
     action: "Visited home page",
   });
 
+  const digitStyle: React.CSSProperties = {
+    fontFamily: "DS-Digital",
+    fontWeight: 400,
+    color: "#f9bc32",
+    fontSize: "3rem",
+    lineHeight: "3rem",
+    top: 0,
+    right: 0,
+    padding: "0 6px",
+    WebkitTextSizeAdjust: "100%",
+    textAlign: "right",
+    boxSizing: "border-box",
+  };
+
   return (
     <>
       <ScrollRestoration />
@@ -28,8 +42,33 @@ export default function Home(props: HomeProps) {
           names as they hit MLB home runs, score NHL goals, dunk in the NBA, and
           score NFL touchdowns. Follow along on Twitter!
         </p>
-        <div>
-          <ScoreBox letters={["A", "B", "C", null, null, null]} />
+        <div
+          style={{
+            lineHeight: 1.15,
+            WebkitTextSizeAdjust: "100%",
+            fontSize: "16px",
+            fontFamily: "Open Sans,sans-serif",
+            boxSizing: "border-box",
+            padding: "0 6px",
+            border: "1px solid #434343",
+            backgroundColor: "#313131",
+            borderRadius: "4px",
+            textAlign: "left",
+            boxShadow: "inset 0 0 15px 0 rgba(0,0,0,.7)",
+            display: "flex",
+            width: "fit-content",
+            flexWrap: "wrap",
+          }}
+        >
+          <div style={digitStyle}>ABCDEF</div>
+          <div
+            style={{
+              ...digitStyle,
+              opacity: 0.08,
+            }}
+          >
+            888888
+          </div>
         </div>
         <div className={styles.mostRecentScoresContainer}>
           <MostRecentScoreboard state={state} />

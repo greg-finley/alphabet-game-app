fe472f79076ac5b8431267e0a7e9294c5ac1c2cd
Greg Finley <gregory.finley@gmail.com>
2023-01-11 13:51:33 -0800

    Put stuff in the center

--- src/components/ScoreboardCard.tsx
+++ src/components/ScoreboardCard.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Avatar from "@mui/material/Avatar";
-import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -29,63 +28,54 @@ export default function ScoreboardCard(props: ScoreboardCardProps) {
         marginBottom: "0.5rem",
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-          justifyContent: "left",
-        }}
-      >
-        <CardContent sx={{ flex: "1 0 auto", textAlign: "left" }}>
-          <Typography variant="h6">
-            <div className="Player-name-and-image">
-              <Avatar
-                src={playerImageSrc(play)}
-                alt={play.player_name}
-                sx={{ marginRight: "0.5rem", bgcolor: "white" }}
-              >
-                {/* Fallback image */}
-                <img
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
-                  }}
-                  src="https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&h=110&w=110&scale=crop"
-                  alt={play.player_name}
-                />
-              </Avatar>
-              {play.player_name}
-            </div>
-          </Typography>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <Typography>{sportScore(play.sport) + "!"}</Typography>
-            <a
-              href={`https://twitter.com/${play.sport}AlphabetGame/status/${play.tweet_id}`}
-              style={{ marginLeft: 4 }}
+      <CardContent sx={{ flex: "1 0 auto", textAlign: "left" }}>
+        <Typography variant="h6">
+          <div className="Player-name-and-image">
+            <Avatar
+              src={playerImageSrc(play)}
+              alt={play.player_name}
+              sx={{ bgcolor: "white" }}
             >
-              <TwitterIcon />
-            </a>
+              {/* Fallback image */}
+              <img
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+                src="https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&h=110&w=110&scale=crop"
+                alt={play.player_name}
+              />
+            </Avatar>
           </div>
-          <Typography>{dayjs().to(dayjs.unix(play.completed_at))}</Typography>
-          <Typography>
-            His name has the letter
-            {play.matching_letters.length === 1 ? "" : "s"} :
-          </Typography>
-          <ScoreBox
-            str={play.matching_letters.join("")}
-            padWithUnderscores={true}
-          />
-          <Typography>Next letter:</Typography>
-          <ScoreBox str={play.next_letter} padWithUnderscores={false} />
-          <Typography>Cycles {play.season_phrase}:</Typography>
-          <ScoreBox
-            str={play.times_cycled.toString()}
-            padWithUnderscores={false}
-          />
-        </CardContent>
-      </Box>
+          <div className="Player-name-and-image">{play.player_name}</div>
+        </Typography>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Typography>{sportScore(play.sport) + "!"}</Typography>
+          <a
+            href={`https://twitter.com/${play.sport}AlphabetGame/status/${play.tweet_id}`}
+            style={{ marginLeft: 4 }}
+          >
+            <TwitterIcon />
+          </a>
+        </div>
+        <Typography>{dayjs().to(dayjs.unix(play.completed_at))}</Typography>
+        <Typography>
+          His name has the letter
+          {play.matching_letters.length === 1 ? "" : "s"} :
+        </Typography>
+        <ScoreBox
+          str={play.matching_letters.join("")}
+          padWithUnderscores={true}
+        />
+        <Typography>Next letter:</Typography>
+        <ScoreBox str={play.next_letter} padWithUnderscores={false} />
+        <Typography>Cycles {play.season_phrase}:</Typography>
+        <ScoreBox
+          str={play.times_cycled.toString()}
+          padWithUnderscores={false}
+        />
+      </CardContent>
     </Card>
   );
 }

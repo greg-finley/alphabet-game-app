7d58e6d80c8fc5265f8de03dead4dec9a513785f
Greg Finley <gregory.finley@gmail.com>
2023-01-14 14:09:37 -0800

    Move TextBox to its own component

--- src/components/ScoreboardCard.tsx
+++ src/components/ScoreboardCard.tsx
@@ -7,6 +7,7 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import ScoreBox from "./ScoreBox";
 import { CardContent } from "@mui/material";
 import AspectRatio from "@mui/joy/AspectRatio";
+import TextBox from "./TextBox";
 
 dayjs.extend(relativeTime);
 
@@ -110,44 +111,6 @@ export default function ScoreboardCard(props: ScoreboardCardProps) {
     </Card>
   );
 }
-
-interface TextBoxProps {
-  text: string[];
-  big?: boolean;
-}
-
-const TextBox = (props: TextBoxProps) => {
-  const { text, big } = props;
-  const { fontSize, padding } = big
-    ? { fontSize: "1.2rem", padding: "0.3rem" }
-    : { fontSize: "0.6rem", padding: "0.25rem" };
-  return (
-    <div className="Centered">
-      <div
-        style={{
-          maxWidth: "90%",
-          display: "inline-block",
-          WebkitTextSizeAdjust: "100%",
-          boxSizing: "border-box",
-          fontWeight: 300,
-          textAlign: "center",
-          fontFamily: "Verdana,sans-serif",
-          fontSize: fontSize,
-          lineHeight: fontSize,
-          backgroundColor: "#00000066",
-          padding: padding,
-          borderRadius: "0.3rem",
-          whiteSpace: "pre-wrap",
-          marginBottom: "0.1rem",
-        }}
-      >
-        {text.map((line, index) => (
-          <div key={index}>{line}</div>
-        ))}
-      </div>
-    </div>
-  );
-};
 
 const playerImageSrc = (play: Play) => {
   switch (play.sport) {
---
+++ src/components/TextBox.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+
+interface TextBoxProps {
+  text: string[];
+  big?: boolean;
+}
+
+export default function TextBox(props: TextBoxProps) {
+  const { text, big } = props;
+  const { fontSize, padding } = big
+    ? { fontSize: "1.2rem", padding: "0.3rem" }
+    : { fontSize: "0.6rem", padding: "0.25rem" };
+  return (
+    <div className="Centered">
+      <div
+        style={{
+          maxWidth: "90%",
+          display: "inline-block",
+          WebkitTextSizeAdjust: "100%",
+          boxSizing: "border-box",
+          fontWeight: 300,
+          textAlign: "center",
+          fontFamily: "Verdana,sans-serif",
+          fontSize: fontSize,
+          lineHeight: fontSize,
+          backgroundColor: "#00000066",
+          padding: padding,
+          borderRadius: "0.3rem",
+          whiteSpace: "pre-wrap",
+          marginBottom: "0.1rem",
+        }}
+      >
+        {text.map((line, index) => (
+          <div key={index}>{line}</div>
+        ))}
+      </div>
+    </div>
+  );
+}

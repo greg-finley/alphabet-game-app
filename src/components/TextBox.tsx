78dac254819f90f6232135c869ec9b55fee86b15
Greg Finley <gregory.finley@gmail.com>
2023-01-15 15:09:13 -0800

    Migrate TextBox to CSS, closes #29

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -7,28 +7,9 @@ interface TextBoxProps {
 
 export default function TextBox(props: TextBoxProps) {
   const { text, big } = props;
-  const { fontSize, padding, textAlign } = big
-    ? { fontSize: "1.2rem", padding: "0.3rem", textAlign: "center" as const }
-    : { fontSize: "0.6rem", padding: "0.25rem", textAlign: "left" as const };
   return (
     <div className="Centered">
-      <div
-        style={{
-          display: "inline-block",
-          WebkitTextSizeAdjust: "100%",
-          boxSizing: "border-box",
-          fontWeight: 300,
-          textAlign: textAlign,
-          fontFamily: "Verdana,sans-serif",
-          fontSize: fontSize,
-          lineHeight: fontSize,
-          backgroundColor: "#00000066",
-          padding: padding,
-          borderRadius: "0.3rem",
-          whiteSpace: "pre-wrap",
-          marginBottom: "0.1rem",
-        }}
-      >
+      <div className={`Text-box${big ? " Text-box-big" : ""}`}>
         {text.map((line, index) => (
           <div key={index}>{line}</div>
         ))}

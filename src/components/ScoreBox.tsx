7a31eafd467b2c7ff5bcaed1794c4e72503d94df
Greg Finley <gregory.finley@gmail.com>
2023-01-16 16:09:03 -0800

    Move scorebox to CSS

--- src/components/ScoreBox.tsx
+++ src/components/ScoreBox.tsx
@@ -8,37 +8,8 @@ interface ScoreBoxProps {
 export default function ScoreBox(props: ScoreBoxProps) {
   const { str } = props;
   return (
-    <div
-      style={{
-        lineHeight: 1.15,
-        WebkitTextSizeAdjust: "100%",
-        boxSizing: "border-box",
-        border: "1px solid #434343",
-        backgroundColor: "#313131",
-        borderRadius: "4px",
-        textAlign: "center",
-        boxShadow: "inset 0 0 15px 0 rgba(0,0,0,.7)",
-        display: "flex",
-        alignItems: "center",
-        width: "fit-content",
-        flexWrap: "wrap",
-        marginTop: "0.3rem",
-      }}
-    >
-      <div
-        style={{
-          fontFamily: "DS-Digital",
-          fontWeight: 400,
-          color: "#d10505",
-          fontSize: "2.5rem",
-          lineHeight: "2.5rem",
-          padding: "0 6px",
-          WebkitTextSizeAdjust: "100%",
-          boxSizing: "border-box",
-        }}
-      >
-        {str}
-      </div>
+    <div className="score-container">
+      <div className="score-text">{str}</div>
     </div>
   );
 }

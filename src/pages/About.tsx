18906577e0da67e41e36862260eca6aab23c6073
Greg Finley <gregory.finley@gmail.com>
2023-01-16 16:57:18 -0800

    Buy a coffee

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,6 +6,24 @@ import ReactGA from "react-ga4";
 import { ScrollRestoration } from "react-router-dom";
 import styles from "./About.module.css";
 
+function Coffee() {
+  return (
+    <a
+      className="buyButton"
+      target="_blank"
+      href="https://www.buymeacoffee.com/gregfinley"
+      rel="noreferrer"
+    >
+      <img
+        className="coffeeImage"
+        src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+        alt="Buy me a coffee"
+      />
+      <span className="coffeeButtonText">Buy me a coffee</span>
+    </a>
+  );
+}
+
 export default function About() {
   ReactGA.event({
     category: "User",
@@ -22,6 +40,8 @@ export default function About() {
           <GitHubLinks />
           <h4 style={{ textAlign: "left" }}>Contact</h4>
           <Contact />
+          <h4 style={{ textAlign: "left" }}>Buy me a coffee</h4>
+          <Coffee />
         </div>
       </div>
     </>
